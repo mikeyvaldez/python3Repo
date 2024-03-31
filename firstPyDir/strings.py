35cedc294536b6d17b32d9423030b767ccf24a60
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-03-31 13:43:27 -0700

    using strings in python

---
+++ firstPyDir/strings.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# taking user input
+
+greeting = "Hello"
+name = input("Please enter your name: ")
+
+
+# if we want a space, we can add that too
+print(greeting + ' ' + name + '!')
